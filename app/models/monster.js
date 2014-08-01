83f5c07369d9e806b908a201f892f046b7c7399b
David Lai <me@dlai0001.com>
2014-08-01 00:27:25 -0700

    roll back files back to start of lesson 1

--- app/models/monster.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  xPosition: DS.attr('number'),
-  yPosition: DS.attr('number'),
-
-  autosubscribe: true // auto subsubscribe to published updates
-});
